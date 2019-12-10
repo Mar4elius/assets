c8475e693edb71d83cc7af4923b3cb53be3053ef
Mar4elius <marauman@gmail.com>
2019-12-09 19:49:10 -0700

    Add vue intstance to app.js file

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,20 @@
-require('./bootstrap');
+// That is were we import Vue to our app
+import Vue from 'Vue';
+import VueRouter from 'vue-router'
+
+import routes from './routes';
+
+// add it as a plugin
+Vue.use(VueRouter);
+
+// Components, Plugin Setups, Etc
+import './bootstrap';
+
+// Main top level Vue instance
+new Vue({
+    el: '#app',
+
+    // Intintiate new instance of the router and pass available routes to it
+    router: new VueRouter(routes)
+
+});
